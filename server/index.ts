0a4decb60ad4fde28402e97bca2fc1aa6e1091a1
Dorian Sarry <doriansarry@yahoo.fr>
2025-09-19 08:33:47 -0400

    Fix: Correction du bug de l'onglet Suivi et amélioration de la gestion de session

--- server/index.ts
+++ server/index.ts
@@ -38,7 +38,7 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     sameSite: 'lax',
-    secure: process.env.NODE_ENV === 'production',
+    secure: false,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
 }));
